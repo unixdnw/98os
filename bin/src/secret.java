fdcb5c2140ad503f17047f10acb1f033f143c04b
Tophatguard <144190236+Tophatguard@users.noreply.github.com>
2024-02-12 17:03:34 -0600

    Add files via upload

---
+++ bin/src/secret.java
@@ -0,0 +1,10 @@
+class secret {
+    public static void main(String[] args) {
+        String secret = "get Trolled";
+        System.out.println("Hello, you found me...");
+        System.out.println("those data miners.");
+        System.out.println("always looking for stuff");
+        System.out.println("I just wanted to let you know ");
+        //System.out.println(secret);
+    }
+}
